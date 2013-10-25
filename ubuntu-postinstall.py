f7eeb63c522c96a16246274098359462db0a620c
Shiquan Wang <shiquanwang@gmail.com>
2013-10-25 11:11:43 +0800

    Add: Add header for the script.

---
+++ ubuntu-postinstall.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# My script for post installation of Ubuntu
+#
+# Syntax: # sudo ./ubuntu-postinstall.py
+#
+# Shiquan Wang <shiquan.wang@philips.com>
+# http://shiquanwang.github.io
+# Distributed under the LGPL version 3 license
+
+
+__appname__ = "ubuntu-postinstall"
+__version__ = "0.1"
+__author__ = "Shiquan Wang <shiquanwang@gmail.com>"
+__license__ = "LGPL v3"
+
+
+"""
+Post installation script for Ubuntu
+"""
