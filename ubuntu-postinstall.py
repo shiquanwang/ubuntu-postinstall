b02ef0ddfe8aeeebecb2ab6dfc29c963ccdeb163
Shiquan Wang <shiquanwang@gmail.com>
2013-11-01 13:31:34 +0800

    Add: Add function to run command.

--- ubuntu-postinstall.py
+++ ubuntu-postinstall.py
@@ -22,6 +22,7 @@ Post installation script for Ubuntu
 
 
 import argparse
+import logging
 import os
 import sys
 
@@ -59,6 +60,73 @@ def isroot():
     Return TRUE if user is root.
     """
     return (os.geteuid() == 0)
+
+
+def showexec(description, command, exitonerror = 0, presskey = 0, waitmessage = "", user = "root"):
+    """
+    Execute a system command with a pretty status display (Running / OK / Warning / Error)
+    By default (exitcode=0), the function did not exit if the command failed
+    """
+
+    if _DEBUG:
+        logging.debug("%s" % description)
+        logging.debug("%s" % command)
+
+    # wait message
+    if (waitmessage == ""):
+        waitmessage = description
+
+    # manage very long description
+    if (len(waitmessage) > 65):
+        waitmessage = waitmessage[0:65] + "..."
+    if (len(description) > 65):
+        description = description[0:65] + "..."
+
+    # display the command
+    if (presskey == 1):
+        status = _("[ ENTER ]")
+    else:
+        status = _("[Running]")
+    statuscolor = colors.BLUE
+    sys.stdout.write(colors.NO + "%s" % waitmessage + statuscolor + "%s" % status.rjust(79-len(waitmessage)) + colors.NO)
+    sys.stdout.flush()
+
+    # wait keypressed (optional)
+    if (presskey == 1):
+        try:
+            tmp_input = input()
+        except:
+            pass
+        input()
+
+    # run the command
+    if (user == "root"):
+        returncode = os.system("/bin/sh -c \"%s\" >> /dev/null 2>&1" % command)
+    else:
+        returncode = os.system("/bin/sh -c \"sudo -u {0} -H {1}\" >> /dev/null 2>&1".format(user, command))
+
+    # display the result
+    if ((returncode == 0) or (returncode == 25600)):
+        status = "[  OK   ]"
+        statuscolor = colors.GREEN
+    else:
+        if exitonerror == 0:
+            status = "[Warning]"
+            statuscolor = colors.ORANGE
+        else:
+            status = "[ Error ]"
+            statuscolor = colors.RED
+
+    sys.stdout.write(colors.NO + "\r%s" % description + statuscolor + "%s\n" % status.rjust(79-len(description)) + colors.NO)
+
+    if _DEBUG:
+        logging.debug(_("Returncode = %d") % retruncode)
+
+    # stop the program if returncode and exitonerror != 0
+    if ((returncode != 0) & (exitonerror != 0)):
+        if _DEBUG:
+            logging.debug(_("Forced to quit"))
+        exit(exitonerror)
 
 
 def main():
